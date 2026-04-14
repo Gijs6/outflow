7defa108a6fc137c1b9db040d93934a027747853
Gijs <gijstenberg@gmail.com>
2026-04-14 20:34:52 +0200

    App and requirements

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, url_for
+
+import os
+
+from dotenv import load_dotenv
+
+from utils.filters import register_filters
+from utils.owm import get_weather, get_location
+
+
+load_dotenv(override=True)
+
+app = Flask(__name__)
+
+app.secret_key = os.getenv("SECRET_KEY", os.urandom(100).hex())
+
+register_filters(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.jinja")
+
+
+@app.route("/weather/<coords>")
+def weather(coords):
+    island_url = url_for("weather_island", coords=coords)
+    return render_template("weather.jinja", island_url=island_url)
+
+
+@app.route("/weather/<coords>/island")
+def weather_island(coords):
+    lat, lon = coords.split(",")
+    data = get_weather(lat, lon)
+    location = get_location(lat, lon)
+    return render_template("weather/island.jinja", weather=data, location=location)
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return render_template("404.html"), 404
+
+# @app.errorhandler(500)
+# def internal_server_error(e):
+#     return render_template("500.html"), 500
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
